5c4d4953b8fcc42885f7165585507e710bb08303
fm946969 <fm946969@hotmail.com>
2025-02-07 15:43:00 +0800

    update write functions for display charaters

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+n_pages = 8 # led-matrix has 7 pages
+cur_page = 0 # initial page
+page_interval = 3000 # in milli-seconds
 
 def show_page(page: number):
     write_cmd("ATfc=(" + str(page) + ")")
@@ -11,14 +14,34 @@ def draw_rect(x: number, y: number, dx: number, dy: number, color: number):
 def draw_pixel(x: number, y: number, color: number):
     write_cmd("ATee=(" + str(x) + "," + str(y) + "," + str(color) + ")")
 def wait_for_response():
-    serial.read_until("E")
-    basic.pause(2)
+    basic.pause(250)
 def clear_screen():
     write_cmd("ATd0=()")
 def draw_line(x: number, y: number, dx: number, dy: number, color: number):
     write_cmd("AT90=(" + str(x) + "," + str(y) + "," + str(x + dx) + "," + str(y + dy) + "," + str(color) + ")")
-serial.redirect(SerialPin.P0, SerialPin.P1, BaudRate.BAUD_RATE115200)
+def draw_char_8x8(line: number, col:number, num: number):
+    write_cmd("AT81=(" + line + "," + col + "," + num + ")")
+def draw_char_8x16(line: number, col:number, num: str):
+    write_cmd("AT82=(" + line + "," + col + "," + num + ")")
+def set_foreground_color(color: number):
+    write_cmd("ATef=(" + color + ")")
 
 def on_forever():
-    pass
-basic.forever(on_forever)
+    global cur_page, page_interval
+    basic.show_number(cur_page)
+    show_page(cur_page) # show one page
+    if cur_page == 0:
+        temperature = input.temperature()
+        d0 = temperature % 10
+        d1 = Math.floor(temperature / 10)
+        set_foreground_color(4)
+        draw_char_8x16(2, 3, str(d1))
+        draw_char_8x16(2, 4, str(d0))
+    basic.pause(page_interval)
+    cur_page = (cur_page + 1) % 8 # advance next page
+
+def main():
+    serial.redirect(SerialPin.P1, SerialPin.P0, BaudRate.BAUD_RATE115200)
+    basic.forever(on_forever)
+
+main()
--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+let n_pages = 8
+//  led-matrix has 7 pages
+let cur_page = 0
+//  initial page
+let page_interval = 3000
+//  in milli-seconds
 function show_page(page: number) {
     write_cmd("ATfc=(" + ("" + page) + ")")
 }
@@ -20,8 +26,7 @@ function draw_pixel(x: number, y: number, color: number) {
 }
 
 function wait_for_response() {
-    serial.readUntil("E")
-    basic.pause(2)
+    basic.pause(250)
 }
 
 function clear_screen() {
@@ -32,7 +37,41 @@ function draw_line(x: number, y: number, dx: number, dy: number, color: number) 
     write_cmd("AT90=(" + ("" + x) + "," + ("" + y) + "," + ("" + (x + dx)) + "," + ("" + (y + dy)) + "," + ("" + color) + ")")
 }
 
-serial.redirect(SerialPin.P0, SerialPin.P1, BaudRate.BaudRate115200)
-basic.forever(function on_forever() {
-    
-})
+function draw_char_8x8(line: number, col: number, num: number) {
+    write_cmd("AT81=(" + line + "," + col + "," + num + ")")
+}
+
+function draw_char_8x16(line: number, col: number, num: string) {
+    write_cmd("AT82=(" + line + "," + col + "," + num + ")")
+}
+
+function set_foreground_color(color: number) {
+    write_cmd("ATef=(" + color + ")")
+}
+
+//  advance next page
+function main() {
+    serial.redirect(SerialPin.P1, SerialPin.P0, BaudRate.BaudRate115200)
+    basic.forever(function on_forever() {
+        let temperature: number;
+        let d0: any;
+        let d1: number;
+        
+        basic.showNumber(cur_page)
+        show_page(cur_page)
+        //  show one page
+        if (cur_page == 0) {
+            temperature = input.temperature()
+            d0 = temperature % 10
+            d1 = Math.floor(temperature / 10)
+            set_foreground_color(4)
+            draw_char_8x16(2, 3, "" + d1)
+            draw_char_8x16(2, 4, "" + d0)
+        }
+        
+        basic.pause(page_interval)
+        cur_page = (cur_page + 1) % 8
+    })
+}
+
+main()
